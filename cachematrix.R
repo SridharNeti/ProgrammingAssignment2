7be3c017949c5f4dc2b18af72c411f2396a4dcaf
SridharNeti <netils.iiitb@gmail.com>
2016-11-08 15:41:10 -0600

    programming assignment cacheMatrix
    Programming assignment for Week3 class

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function cache's matrix and set to environment
 
 makeCacheMatrix <- function(x = matrix()) {
+	mtrx <- NULL
+	set <- function(y){
+		x <<- y
+		mtrx <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) mtrx <<- solve
+	getinverse <- function() mtrx
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## this function checks the environment if inverse already exists if so returns that otherwise 
+## matrix is inversed and set to cache
 
 cacheSolve <- function(x, ...) {
+	m <- x$getinverse()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
         ## Return a matrix that is the inverse of 'x'
+	m
 }
